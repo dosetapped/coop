d5b671959f9f585e29693adec8d7f695fe51b806
dosetapped <mike.abifadel@gmail.com>
2026-09-18 14:29:14 +1000

    m2, steam api script

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+import requests
+from dotenv import load_dotenv
+import os
 
-app = Flask(__name__)
+load_dotenv()
 
-@app.route('/')
-def home():
-    return 'Hello, world!'
+api_key = os.getenv("STEAM_API_KEY")
+steam_id = "76561199125657263"  # replace with your real 17-digit SteamID64
 
-if __name__ == '__main__':
-    app.run(debug=True)
+url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
+
+params = {
+    "key": api_key,
+    "steamid": steam_id,
+    "format": "json",
+    "include_appinfo": True,
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+games = data["response"]["games"]
+
+for game in games:
+    print(game["name"], game["playtime_forever"] / 60)
